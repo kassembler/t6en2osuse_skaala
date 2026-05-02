ed7cd64ccc8259a35da742cf4678a74dbc385d49
kassembler <mirjam.kristi@gmail.com>
2026-05-02 10:40:28 +0300

    Rename kujuta_graafikul.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QFont
 import pyqtgraph as pg
-from PyQt5.QtGui import *
 
 from hangi_andmed import hangi_t6en2osused
 
@@ -13,25 +13,25 @@ class ProbabilityLine(QtWidgets.QMainWindow):
         self.setWindowTitle("Tõenäosuste skaala")
         self.setGeometry(100, 100, 1000, 300)
 
+        self.init_ui()
+
+    def init_ui(self):
         data = hangi_t6en2osused()
+        data.sort(key=lambda x: x[1]) #Sorteeri andmed suuruse järgi
 
-        data.sort(key=lambda x: x[1]) #Sorteeri t6en2osused suuruse j2rgi
-
-        labels = [item[0] for item in data]
         values = [item[1] for item in data]
 
         self.plot_widget = pg.PlotWidget()
         self.setCentralWidget(self.plot_widget)
+        pg.setConfigOptions(antialias=True)
 
-        self.plot_widget.hideAxis('left') #Peidame Y telje
-        self.plot_widget.setXRange(0, 1) #Tõenäosus on min 0 ja max 1
+        self.plot_widget.hideAxis('left')
+        self.plot_widget.setXRange(0, 1)
         self.plot_widget.setLimits(xMin=0, xMax=1)
-
-        self.plot_widget.setLabel('bottom', 'Tõenäosus (0 → 1)')
+        self.plot_widget.setLabel('bottom', 'Tõenäosus (0 → 1)') #Tõenäosus saab olla vaid 0 kuni 1
 
         self.plot_widget.plot([0, 1], [0, 0], pen=pg.mkPen(width=2))
 
-        #Punktid teljel
         scatter = pg.ScatterPlotItem(
             x=values,
             y=[0] * len(values),
@@ -40,21 +40,27 @@ class ProbabilityLine(QtWidgets.QMainWindow):
         )
         self.plot_widget.addItem(scatter)
 
-        #Selgitav tekst iga punkti juures
-        font = QFont('Arial', 11)
+        font = QFont('Arial', 10) #Font
+
         for i, (label, value) in enumerate(data):
+            y_offset = 0.03 + i * 0.03 #Selleks, et tekstid kattuksid vähem
+
             text = pg.TextItem(
                 text=f"{label}\n({value:.4f})",
-                anchor=(0, 0),
-                #angle=75
-                
+                anchor=(0, 0)
             )
-            y_offset = 0.02 + (i % 3) * 0.02
-            text.setPos(value, y_offset)
-            self.plot_widget.addItem(text)
             text.setFont(font)
-            line = pg.PlotDataItem(x=[value, value], y=[0, y_offset], pen=pg.mkPen(color=(150, 150, 150), width=1))
+            text.setPos(value, y_offset)
+
+            self.plot_widget.addItem(text)
+
+            line = pg.PlotDataItem(
+                x=[value, value],
+                y=[0, y_offset],
+                pen=pg.mkPen(color=(150, 150, 150), width=1)
+            )
             self.plot_widget.addItem(line)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
